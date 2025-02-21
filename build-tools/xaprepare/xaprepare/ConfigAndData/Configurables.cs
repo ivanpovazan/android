681f2158883bd2b8d2415109e567cbc8d313885e
Ivan Povazan <ivan.povazan@gmail.com>
2025-02-21 11:56:43 +0100

    Fix pulling in shipped coreclr packages

--- build-tools/xaprepare/xaprepare/ConfigAndData/Configurables.cs
+++ build-tools/xaprepare/xaprepare/ConfigAndData/Configurables.cs
@@ -255,11 +255,9 @@ namespace Xamarin.Android.Prepare
 					return Path.Combine (localClrDir, "runtimes", $"android-{androidTarget}");
 				}
 
-				// TODO: The nuget id and the ref package version are guesses atm, since the CoreCLR packages don't exist yet
-				Log.Instance.Todo ("The nuget id and the ref package version are guesses atm, since the CoreCLR packages don't exist yet");
 				return Path.Combine (
 					XAPackagesDir,
-					$"microsoft.netcore.app.runtime.coreclr.android-{androidTarget}",
+					$"microsoft.netcore.app.runtime.android-{androidTarget}",
 					ctx.Properties.GetRequiredValue (KnownProperties.MicrosoftNETCoreAppRefPackageVersion),
 					"runtimes",
 					$"android-{androidTarget}"
